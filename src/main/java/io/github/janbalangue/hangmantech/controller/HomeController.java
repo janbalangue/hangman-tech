967ce601551d3225668db24ea1aaefc4c859f6b7
Jan Balangue <janbalangue@Jans-MacBook-Air.local>
2022-11-12 18:01:50 -0600

    Add youtube links

--- src/main/java/io/github/janbalangue/hangmantech/controller/HomeController.java
+++ src/main/java/io/github/janbalangue/hangmantech/controller/HomeController.java
@@ -1,11 +1,10 @@
 package io.github.janbalangue.hangmantech.controller;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import io.github.janbalangue.hangmantech.entity.Solution;
 import io.github.janbalangue.hangmantech.repository.SolutionRepository;
 
 @RestController
@@ -23,16 +21,31 @@ public class HomeController implements ErrorController {
     @Autowired
     private SolutionRepository solutionRepository;
 
+    Map<String, String> solutionMap;
+    ModelAndView modelAndView;
+
     @GetMapping("/")
     public ModelAndView home() {
-        ModelAndView modelAndView = new ModelAndView();
+        modelAndView = new ModelAndView();
         modelAndView.setViewName("home.html");
         return modelAndView;
     }
 
     @PostMapping()
-    public String getSolution(Model model) {
-        return solutionRepository.findSolution();
-    }    
+    public ModelAndView getSolutionAndStartGame(Model model) {
+        getSolution();
+        modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:" + solutionMap.get("youtube"));
+        return modelAndView;
+    }
+    
+    public void getSolution() {
+        String solution = solutionRepository.findSolution();
+        String[] solutionParts = solution.split(",");
+        solutionMap = new HashMap<>();
+        solutionMap.put("name", solutionParts[1]);
+        solutionMap.put("wiki", solutionParts[2]);
+        solutionMap.put("youtube", solutionParts[3]);
+    }
     
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,24 @@
-INSERT INTO solutions(name, url) VALUES('inheritance', 'https://en.wikipedia.org/wiki/Inheritance_(object-oriented_programming)');
-INSERT INTO solutions(name, url) VALUES('encapsulation', 'https://en.wikipedia.org/wiki/Encapsulation_(computer_programming)');
-INSERT INTO solutions(name, url) VALUES('polymorphism', 'https://en.wikipedia.org/wiki/Polymorphism_(computer_science)');
-INSERT INTO solutions(name, url) VALUES('abstraction', 'https://en.wikipedia.org/wiki/Abstraction_(computer_science)');
-INSERT INTO solutions(name, url) VALUES('multithreading', 'https://en.wikipedia.org/wiki/Thread_(computing)');
-INSERT INTO solutions(name, url) VALUES('refactoring', 'https://en.wikipedia.org/wiki/Code_refactoring');
-INSERT INTO solutions(name, url) VALUES('microservices', 'https://en.wikipedia.org/wiki/Microservices');
-INSERT INTO solutions(name, url) VALUES('recursion', 'https://en.wikipedia.org/wiki/Recursion');
+INSERT INTO solutions(name, wiki, youtube) VALUES('inheritance', 
+'https://en.wikipedia.org/wiki/Inheritance_(object-oriented_programming)',
+'https://youtu.be/V4EWZv0tev8');
+INSERT INTO solutions(name, wiki, youtube) VALUES('encapsulation', 
+'https://en.wikipedia.org/wiki/Encapsulation_(computer_programming)',
+'https://youtu.be/cU94So54cr8');
+INSERT INTO solutions(name, wiki, youtube) VALUES('polymorphism', 
+'https://en.wikipedia.org/wiki/Polymorphism_(computer_science)',
+'https://youtu.be/Ft88V_rDO4I');
+INSERT INTO solutions(name, wiki, youtube) VALUES('abstraction', 
+'https://en.wikipedia.org/wiki/Abstraction_(computer_science)',
+'https://youtu.be/52frlN8webg');
+INSERT INTO solutions(name, wiki, youtube) VALUES('multithreading', 
+'https://en.wikipedia.org/wiki/Thread_(computing)',
+'https://youtu.be/x5akmCWgGY0');
+INSERT INTO solutions(name, wiki, youtube) VALUES('refactoring', 
+'https://en.wikipedia.org/wiki/Code_refactoring',
+'https://youtu.be/p-oWHEfXEVs');
+INSERT INTO solutions(name, wiki, youtube) VALUES('microservices', 
+'https://en.wikipedia.org/wiki/Microservices',
+'https://youtu.be/j1gU2oGFayY');
+INSERT INTO solutions(name, wiki, youtube) VALUES('recursion', 
+'https://en.wikipedia.org/wiki/Recursion',
+'https://youtu.be/sE0sH8vSmE4');
